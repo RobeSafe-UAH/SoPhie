3c0e6e59ab7d3b330515c2e5e5f258a16f4859ae
Cram3r95 <cram3r95@gmail.com>
2021-09-08 15:37:52 +0200

    aiodrive evaluation with model v2 (95 % training)

--- check_model.sh
+++ check_model.sh
@@ -1,28 +1,29 @@
 #!/usr/bin/env bash 
 
-model_path="test_with_model.pt"  # $1
+evaluate_model="evaluate/evaluate_model_aiodrive.py"
+model_path="save/test_with_model_95_percent_labels.pt"  # $1
 results_file="final_submission_v50" # $2
 
 # Car
 
-python3.8 evaluate_model_aiodrive.py --model_path=$model_path --dataset_path=data/datasets/aiodrive/aiodrive_Car/ --results_file=$results_file --skip=1
-python3.8 evaluate_model_aiodrive.py --model_path=$model_path --dataset_path=data/datasets/aiodrive/aiodrive_Car/ --results_file=$results_file --skip=2
-python3.8 evaluate_model_aiodrive.py --model_path=$model_path --dataset_path=data/datasets/aiodrive/aiodrive_Car/ --results_file=$results_file --skip=5
+python3.8 $evaluate_model --model_path=$model_path --dataset_path=data/datasets/aiodrive/aiodrive_Car/ --results_file=$results_file --skip=1
+# python3.8 evaluate/evaluate_model_aiodrive.py --model_path=$model_path --dataset_path=data/datasets/aiodrive/aiodrive_Car/ --results_file=$results_file --skip=2
+# python3.8 evaluate/evaluate_model_aiodrive.py --model_path=$model_path --dataset_path=data/datasets/aiodrive/aiodrive_Car/ --results_file=$results_file --skip=5
 
-# Cyclist 
+# # Cyclist 
 
-# python3.8 evaluate_model_aiodrive.py --model_path=$model_path --dataset_path=data/datasets/aiodrive/aiodrive_Cyc/ --results_file=$results_file --skip=1
-# python3.8 evaluate_model_aiodrive.py --model_path=$model_path --dataset_path=data/datasets/aiodrive/aiodrive_Cyc/ --results_file=$results_file --skip=2
-# python3.8 evaluate_model_aiodrive.py --model_path=$model_path --dataset_path=data/datasets/aiodrive/aiodrive_Cyc/ --results_file=$results_file --skip=5
+# python3.8 evaluate/evaluate_model_aiodrive.py --model_path=$model_path --dataset_path=data/datasets/aiodrive/aiodrive_Cyc/ --results_file=$results_file --skip=1
+# python3.8 evaluate/evaluate_model_aiodrive.py --model_path=$model_path --dataset_path=data/datasets/aiodrive/aiodrive_Cyc/ --results_file=$results_file --skip=2
+# python3.8 evaluate/evaluate_model_aiodrive.py --model_path=$model_path --dataset_path=data/datasets/aiodrive/aiodrive_Cyc/ --results_file=$results_file --skip=5
 
-# Motorcycle
+# # Motorcycle
 
-# python3.8 evaluate_model_aiodrive.py --model_path=$model_path --dataset_path=data/datasets/aiodrive/aiodrive_Mot/ --results_file=$results_file --skip=1
-# python3.8 evaluate_model_aiodrive.py --model_path=$model_path --dataset_path=data/datasets/aiodrive/aiodrive_Mot/ --results_file=$results_file --skip=2
-# python3.8 evaluate_model_aiodrive.py --model_path=$model_path --dataset_path=data/datasets/aiodrive/aiodrive_Mot/ --results_file=$results_file --skip=5
+# python3.8 evaluate/evaluate_model_aiodrive.py --model_path=$model_path --dataset_path=data/datasets/aiodrive/aiodrive_Mot/ --results_file=$results_file --skip=1
+# python3.8 evaluateevaluate_model_aiodrive.py --model_path=$model_path --dataset_path=data/datasets/aiodrive/aiodrive_Mot/ --results_file=$results_file --skip=2
+# python3.8 evaluate/evaluate_model_aiodrive.py --model_path=$model_path --dataset_path=data/datasets/aiodrive/aiodrive_Mot/ --results_file=$results_file --skip=5
 
-# Pedestrian 
+# # Pedestrian 
 
-# python3.8 evaluate_model_aiodrive.py --model_path=$model_path --dataset_path=data/datasets/aiodrive/aiodrive_Ped/ --results_file=$results_file --skip=1
-# python3.8 evaluate_model_aiodrive.py --model_path=$model_path --dataset_path=data/datasets/aiodrive/aiodrive_Ped/ --results_file=$results_file --skip=2
-# python3.8 evaluate_model_aiodrive.py --model_path=$model_path --dataset_path=data/datasets/aiodrive/aiodrive_Ped/ --results_file=$results_file --skip=5
+# python3.8 evaluate/evaluate_model_aiodrive.py --model_path=$model_path --dataset_path=data/datasets/aiodrive/aiodrive_Ped/ --results_file=$results_file --skip=1
+# python3.8 evaluate/evaluate_model_aiodrive.py --model_path=$model_path --dataset_path=data/datasets/aiodrive/aiodrive_Ped/ --results_file=$results_file --skip=2
+# python3.8 evaluate/evaluate_model_aiodrive.py --model_path=$model_path --dataset_path=data/datasets/aiodrive/aiodrive_Ped/ --results_file=$results_file --skip=5
